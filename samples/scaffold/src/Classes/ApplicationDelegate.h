ab001d332f1cbee7fc7ab6269eaee96a57bd2214
PrimaryFeather <redge@incognitek.com>
2011-01-04 15:20:33 +0000

    moved import of "Sparrow.h" to precompiled header

--- samples/scaffold/src/Classes/ApplicationDelegate.h
+++ samples/scaffold/src/Classes/ApplicationDelegate.h
@@ -7,7 +7,6 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "Sparrow.h" 
 
 @interface ApplicationDelegate : NSObject <UIApplicationDelegate> 
 {
--- samples/scaffold/src/Classes/Game.h
+++ samples/scaffold/src/Classes/Game.h
@@ -7,7 +7,6 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "Sparrow.h" 
 
 @interface Game : SPStage
 
