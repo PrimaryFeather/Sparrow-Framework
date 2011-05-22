7a415684a7b63b79bbe487fb1ebc81c18287b21b
PrimaryFeather <redge@incognitek.com>
2011-05-22 14:17:09 +0200

    raised version to 1.2

--- sparrow/src/Classes/Sparrow.h
+++ sparrow/src/Classes/Sparrow.h
@@ -9,7 +9,7 @@
 //  it under the terms of the Simplified BSD License.
 //
 
-#define SPARROW_VERSION @"1.1.0"
+#define SPARROW_VERSION @"1.2"
 
 #import "SPNSExtensions.h"
 #import "SPEventDispatcher.h"
