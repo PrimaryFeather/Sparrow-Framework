05796f251777eb62c95a584d5f817360004d66a1
Daniel Sperl <daniel@gamua.com>
2012-03-01 08:55:10 +0100

    raised version to 1.3

--- sparrow/src/Classes/Sparrow.h
+++ sparrow/src/Classes/Sparrow.h
@@ -9,7 +9,7 @@
 //  it under the terms of the Simplified BSD License.
 //
 
-#define SPARROW_VERSION @"1.2"
+#define SPARROW_VERSION @"1.3"
 
 #import "SPNSExtensions.h"
 #import "SPEventDispatcher.h"
