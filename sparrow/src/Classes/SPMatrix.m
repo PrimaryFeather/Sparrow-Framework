19f1766941f8d1451b758b946056bda51747db15
Daniel Sperl <daniel@gamua.com>
2012-05-29 09:57:11 +0200

    optimized rotation method

--- sparrow/src/Classes/SPMatrix.m
+++ sparrow/src/Classes/SPMatrix.m
@@ -94,10 +94,12 @@ static void setValues(SPMatrix *matrix, float a, float b, float c, float d, floa
 
 - (void)rotateBy:(float)angle
 {
-    SPMatrix *rotMatrix = [[SPMatrix alloc] initWithA:cosf(angle) b:sinf(angle)
-                                                    c:-sinf(angle) d:cosf(angle) tx:0 ty:0];
-    [self concatMatrix:rotMatrix];
-    [rotMatrix release];    
+    float cos = cosf(angle);
+    float sin = sinf(angle);
+    
+    setValues(self, mA*cos  - mB*sin,    mA*sin  + mB*cos, 
+                    mC*cos  - mD*sin,    mC*sin  + mD*cos, 
+                    mTx*cos - mTy * sin, mTx*sin + mTy*cos);
 }
 
 - (void)identity
